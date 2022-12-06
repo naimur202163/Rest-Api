a34baeed4673b2e04f19ce1bdece4e973caa92a8
naimur <naimur202163@gmail.com>
2022-12-06 17:13:18 +0600

    working with uuid

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,28 @@ const express = require("express");
 // const router = express.Router();
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "Naimur Rahman",
+    lastName: "Rahman",
+    age: 21,
+  },
+  {
+    firstName: "Rny",
+    lastName: "d",
+    age: 31,
+  },
+];
+
 router.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send(users);
 });
 
-module.exports=router
+router.post("/", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  console.log(user)
+  res.send(`User With UserName ${user.fristName} add to the database`)
+});
+
+module.exports = router;

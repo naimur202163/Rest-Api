cb67011a263219f20ed41f5dbdf99134fbd78f07
naimur <naimur202163@gmail.com>
2022-12-06 16:47:51 +0600

    all routes are working

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Naimur Rhman d")
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 4000;
+
+app.use(bodyParser.json());
+
+// Routes
+
+app.get("/", (req, res) => {
+  console.log("TEst");
+  res.send("HEllo From The Server");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on htts:localhost${PORT}`);
+});

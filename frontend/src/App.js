abb6fdb165275d21f413d3384ba238a2fc4f893e
naimur <naimur202163@gmail.com>
2022-12-08 18:59:38 +0600

    working with footer section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,33 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { BrowserRouter, Routes ,Link} from "react-router-dom";
+import { BrowserRouter, Routes, Link } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import "./App.css";
 import ProductScreen from "./screens/ProductScreen";
+import { Container, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 export default function App() {
   return (
     <BrowserRouter>
       <div>
-        <header>
-          <Link to="/">Amozona</Link>
-        </header>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>Amozona</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
+
         <main>
           <Routes>
             <Route path="/product/:slug" element={<ProductScreen />} />
             <Route path="/" element={<HomeScreen />}></Route>
           </Routes>
         </main>
+
+        <footer>
+          
+        </footer>
       </div>
     </BrowserRouter>
   );

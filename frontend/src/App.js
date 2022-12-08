8f9c7da7c08984b3c199708fa71184dfbb1c50f3
naimur <naimur202163@gmail.com>
2022-12-08 14:31:42 +0600

    working on products route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import data from "./data";
 
 export default function App() {
-  return <div>
-
-    <header >
-      <a href="/">Amozona</a>
-    </header>
-  </div>;
+  return (
+    <div>
+      <header>
+        <a href="/">Amozona</a>
+      </header>
+      <main>
+        <h1>Featured Products</h1>
+        {data.products.map((product) => (
+          <div>
+            <img src={product.image} alt={product.name} />
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
 }

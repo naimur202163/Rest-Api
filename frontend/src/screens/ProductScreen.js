3f0dac3591c2f7fa621396a1924e96312e603ac5
naimur <naimur202163@gmail.com>
2022-12-08 21:58:00 +0600

    working with slug

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,11 @@
-import React from "react";
+import { React, useReducer, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { axios } from "axios";
+import { Link } from "react-router-dom";
 
 export default function ProductScreen() {
   const params = useParams();
   const { slug } = params;
-  return <div>
-    {slug }
-  </div>;
+
+  return <>{slug}</>;
 }

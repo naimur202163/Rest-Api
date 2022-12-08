76dafc6b7524dbd34d036460df8f4c6fede4de70
naimur <naimur202163@gmail.com>
2022-12-08 14:46:06 +0600

    home screen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,44 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
 import data from "./data";
+import HomeScreen from "./screens/HomeScreen";
 export default function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Amozona</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
+    <BrowserRouter>
+      <div>
+        <header>
+          <a href="/">Amozona</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />}></Route>
+          </Routes>
+          <h1>Featured Products</h1>
 
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-
-              <div className="product-info">
+          <div className="products">
+            {data.products.map((product) => (
+              <div className="product" key={product.slug}>
                 <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
+                  <img src={product.image} alt={product.name} />
                 </a>
 
-                <p>
-                  {" "}
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+
+                  <p>
+                    {" "}
+                    <strong>${product.price}</strong>
+                  </p>
+                  <button>Add to cart</button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+            ))}
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }

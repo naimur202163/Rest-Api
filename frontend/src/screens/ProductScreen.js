e42f437cc045b585ffebb0956809908c323f69ca
naimur <naimur202163@gmail.com>
2022-12-08 14:52:22 +0600

    product details page added

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function ProductScreen() {
-  return (
-    <div>ProductScreen</div>
-  )
+  const params = useParams();
+  const { slug } = params;
+  return <div>
+    {slug }
+  </div>;
 }

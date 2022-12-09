eba6e7fa1ce517b7c793ecfe9e7fd1f4f4e9cddf
naimur <naimur202163@gmail.com>
2022-12-09 14:54:47 +0600

    error solved

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,66 @@
 import { React, useReducer, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { axios } from "axios";
-import { Link } from "react-router-dom";
+import axios from "axios";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        product: action.payload,
+        loading: false,
+      };
+
+    case "FETCH_FAIL":
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
 export default function ProductScreen() {
   const params = useParams();
   const { slug } = params;
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: "",
+  });
 
-  return <>{slug}</>;
+  // Reducer Hook
+
+  // Fetching Data
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+
+      try {
+        const result = await axios.get(`/api/products/slug/${slug}`);
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+
+        console.log(result.data);
+      } catch (error) {
+        dispatch({ type: "FETCH_FAIL", payload: error.message });
+      }
+    };
+    fetchData();
+  }, [slug]);
+
+  return loading ? (
+    <div>Loading......</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
+    <div>
+        {product.name}
+    </div>
+  );
 }

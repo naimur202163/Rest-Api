9e5adbd5554ae4af24ca05604c2318b6d377360c
naimur <naimur202163@gmail.com>
2022-12-10 14:52:17 +0600

    data loaded successfully

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,23 @@
-import React from "react";
-import data from "../data";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import ProductsScreen from "./ProductsScreen";
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(`/api/products`);
+      setProducts(data);
+    };
+
+   
+    fetchData();
+  }, []);
   return (
     <div>
       <div className="content">
         <ul className="products">
-          {data.products.map((product) => (
+          {products.map((product) => (
             <li>
               <ProductsScreen product={product}></ProductsScreen>
             </li>

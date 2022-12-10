ac06b1c9a723c3ce791926bcf8882003a92724fc
naimur <naimur202163@gmail.com>
2022-12-11 00:32:22 +0600

    productRoute is addd

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
 import express from "express";
 import data from "./data.js";
+const port = 5000;
 const app = express();
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
-
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({
+      message: "Product Not Found",
+    });
+  }
+});
 app.get("/", (req, res) => {
   res.send("Hello From the server");
 });
 
-app.listen(5000, () => {
-  console.log(`The server is running on port 5000`);
+app.listen(port, () => {
+  console.log(`The server is running on port ${port}`);
 });
